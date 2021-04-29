e7b441494ebf2e3e0eb5b8836bbb980550235c10
rafaballerini <rafaella_ballerini@hotmail.com>
2021-04-29 18:50:04 -0300

    melhoria do erro

--- Classes/Gamification.py
+++ Classes/Gamification.py
@@ -5,29 +5,29 @@ repository = Repository()
 class Gamification:
 
     def channel_reaction(self, user_id):
-        value = 100
-        data = {
-            'user_id': user_id,
-            'value': value
-        }
-        repository.insert_one_xp(data)
+        # value = 100
+        # data = {
+        #     'user_id': user_id,
+        #     'value': value
+        # }
+        # repository.insert_one_xp(data)
         return False
 
     def nice_reaction(self, user_id):
-        value = 200
-        data = {
-            'user_id': user_id,
-            'value': value
-        }
-        repository.update_one_xp(data)
+        # value = 200
+        # data = {
+        #     'user_id': user_id,
+        #     'value': value
+        # }
+        # repository.update_one_xp(data)
         return False
         
         
     def bad_reaction(self, user_id):
-        value = -200
-        data = {
-            'user_id': user_id,
-            'value': value
-        }
-        repository.update_one_xp(data)
+        # value = -200
+        # data = {
+        #     'user_id': user_id,
+        #     'value': value
+        # }
+        # repository.update_one_xp(data)
         return False
